27af717d489a72ceaf3e77838481044453ce00da
estherjsuh <estherjsuh@gmail.com>
2020-06-08 09:55:06 -0700

    Updates

---
+++ precache-manifest.a2e852ef0679d23f59f5f0de790294e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c0e38f7e6af985bc5471582e3cb2ee",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cad1ed7bdfd7b00778f5",
+    "url": "/monsters-rolodex/static/css/main.ea1c1982.chunk.css"
+  },
+  {
+    "revision": "a18d43196547948386f9",
+    "url": "/monsters-rolodex/static/js/2.8ebdf4f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8ebdf4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad1ed7bdfd7b00778f5",
+    "url": "/monsters-rolodex/static/js/main.608e9b33.chunk.js"
+  },
+  {
+    "revision": "345913ed4d11f5710073",
+    "url": "/monsters-rolodex/static/js/runtime-main.09132acd.js"
+  }
+]);
